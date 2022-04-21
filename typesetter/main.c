da721e46734850857bcc021a105e4d15e79e0c4b
Your Name <you@example.com>
2022-04-21 13:05:01 +0600

    1.3.2 + проверка символов введенного слова с символами в другом слове

--- typesetter/main.c
+++ typesetter/main.c
@@ -5,40 +5,46 @@
 #include <string.h>
 
 char* getRandomWord(){
-    char str[50];                                       // Слово из словаря [длина слова]
-    FILE *vocabulary;                                   // Подключение файла
+    char str[50];                                       // *Слово из словаря [длина слова]
+    FILE *vocabulary;                                   // *Подключение файла
     vocabulary = fopen("/Users/dmitry/Desktop/ОмГУПС/typesetter/russian.txt","r"); //[!]Сделать относительный путь
-    fseek(vocabulary, rand() % 8366227, SEEK_SET);      // Смещение указателя на случайное количество байт !> 8366227 – число байт
+    fseek(vocabulary, rand() % 8366227, SEEK_SET);      // *Смещение указателя на случайное количество байт !> 8366227 – число байт
     if (NULL != fgets(str, 100, vocabulary)){
-        fscanf(vocabulary, "%s",&str);                  // Чтение строки
+        fscanf(vocabulary, "%s",&str);                  // *Чтение строки
         //printf("%s\n",str);
 
     }
-    fclose(vocabulary);                                 // Закрытие потока
+    fclose(vocabulary);                                 // *Закрытие потока
     return str;
 }
 
 /*char checkInput(playerInput){
-    printf("Проверка слова%s\n",playerInput);           //[!] Без %s работает, надо впихнуть \0
+    printf("Проверка слова%s\n",playerInput);           // ⚠️ Segmentation fault | [!] Без %s работает, надо впихнуть \0
 }*/
 
 char startGame(){
-    char randomWord[50] = {getRandomWord()};        //[!!!] Вывод функции в переменную, strchr
-    printf(">%s<\n",randomWord);                   // Вывод рандомного слова
-    printf("Введите слово\n");
-    char playerInput[50]={0};                              // Строка для ввода слова игроком
-    scanf("%s\0",playerInput);                         // [!]При стирании символы запоминаются
+    char randomWord[50] = "getRandomWord"/*{getRandomWord()}*/; // ⚠️ Нужен вывод функции в переменную strchr, чтобы не вызывать каждый раз getRandomWord
+    printf("Случанйое слово: %s\n",getRandomWord());    // *Вывод рандомного слова
+    printf("Введите слово: ");
+    char playerInput[50]={0};                           // *Строка для ввода слова игроком
+    scanf("%s\0",playerInput);                          // ⚠️ При стирании символы запоминаются
     printf("Вы ввели слово: %s\n",playerInput);
     //checkInput(playerInput);
+    //Это перенести в checkInput:
     
     for(int i=0; playerInput[i]; i++){
-        printf("[%d]=%c\n",i, playerInput[i]);
-        //if(strchr())
+        //printf("[%d]=%c\n",i, playerInput[i]);        // (Посимвольный вывод playerInput)
+        if(strchr(/*getRandomWord()*/randomWord,playerInput[i])){  // ⚠️ Чувствителен к регистру | Поиск (в строке, символа)
+            printf("%c – подходит!\n",playerInput[i]);  // Покрасить вывод
+        }
+        else{
+            printf("[!] В слове %s не содержится буква %c\n",randomWord,playerInput[i]);
+        }
     }
 }
 
 void main() {
-    setlocale(LC_ALL, "Russian");
+    //setlocale(LC_ALL, "Russian");
     srand(time(NULL));
     startGame();
 }
