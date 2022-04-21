5b1d40fa9ca5c70e944dfb794d733a41edb5ceaf
Your Name <you@example.com>
2022-04-21 09:27:50 +0600

    1.3.1 checkInput в main, выводятся посимвольно только латинские символы

--- typesetter/main.c
+++ typesetter/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
+#include <string.h>
 
 char* getRandomWord(){
     char str[50];                                       // Слово из словаря [длина слова]
     FILE *vocabulary;                                   // Подключение файла
-    //setlocale(LC_ALL, "Russian");
     vocabulary = fopen("/Users/dmitry/Desktop/ОмГУПС/typesetter/russian.txt","r"); //[!]Сделать относительный путь
     fseek(vocabulary, rand() % 8366227, SEEK_SET);      // Смещение указателя на случайное количество байт !> 8366227 – число байт
     if (NULL != fgets(str, 100, vocabulary)){
@@ -17,20 +18,27 @@ char* getRandomWord(){
     return str;
 }
 
-char checkInput(playerInput){
+/*char checkInput(playerInput){
     printf("Проверка слова%s\n",playerInput);           //[!] Без %s работает, надо впихнуть \0
-}
+}*/
 
 char startGame(){
-    printf(">%s<\n",getRandomWord());                   // Вывод рандомного слова
-    //printf("Введите слово\n");
-    char playerInput[50];                                     // Строка для ввода слова игроком
-    scanf("%s\0",&playerInput);
-    printf("Вы ввели слово:\n%s\n",playerInput);
-    checkInput(playerInput);
+    char randomWord[50] = {getRandomWord()};        //[!!!] Вывод функции в переменную, strchr
+    printf(">%s<\n",randomWord);                   // Вывод рандомного слова
+    printf("Введите слово\n");
+    char playerInput[50]={0};                              // Строка для ввода слова игроком
+    scanf("%s\0",playerInput);                         // [!]При стирании символы запоминаются
+    printf("Вы ввели слово: %s\n",playerInput);
+    //checkInput(playerInput);
+    
+    for(int i=0; playerInput[i]; i++){
+        printf("[%d]=%c\n",i, playerInput[i]);
+        //if(strchr())
+    }
 }
 
 void main() {
+    setlocale(LC_ALL, "Russian");
     srand(time(NULL));
     startGame();
 }
