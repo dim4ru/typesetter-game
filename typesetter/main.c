33f5bfe48e3772b10764bfb156edfa62cae7f845
Your Name <you@example.com>
2022-04-26 12:22:57 +0600

    [1.4.2] После правок

--- typesetter/main.c
+++ typesetter/main.c
@@ -1,3 +1,4 @@
+//cp866 кодировку использовать
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -10,7 +11,7 @@ char* getRandomWord(){
     vocabulary = fopen("/Users/dmitry/Desktop/ОмГУПС/typesetter/russian.txt","r"); //[!]Сделать относительный путь
     fseek(vocabulary, rand() % 8366227, SEEK_SET);      // *Смещение указателя на случайное количество байт !> 8366227 – число байт
     if (NULL != fgets(str, 100, vocabulary)){
-        fscanf(vocabulary, "%s",&str);                  // *Чтение строки
+        fscanf(vocabulary, "%s",str);                  // *Чтение строки
         //printf("%s\n",str);
 
     }
@@ -18,9 +19,9 @@ char* getRandomWord(){
     return str;
 }
 
-/*char checkInput(playerInput){
+char checkInput(char* playerInput){
     printf("Проверка слова%s\n",playerInput);           // ⚠️ Segmentation fault | [!] Без %s работает, надо впихнуть \0
-}*/
+}
 
 char startGame(){
     char randomWord[50] = "getRandomWord"/*{getRandomWord()}*/; // ⚠️ Нужен вывод функции в переменную strchr, чтобы не вызывать каждый раз getRandomWord
@@ -29,23 +30,24 @@ char startGame(){
     char playerInput[50]={0};                           // *Строка для ввода слова игроком
     scanf("%s",&playerInput);                           // [!] Проверка на пустую строку
     printf("Вы ввели слово: %s\n",playerInput);
-    //checkInput(playerInput);
+    checkInput(playerInput);                          // 👁👁👁
     
     
     
     // Это перенести в checkInput:
     
     // Сравнение букв в playerInput с буквами randomWord
+    
     int isCorrect;                                      // [?] int вместо bool
     for(int i=0; playerInput[i]; i++){
-        //printf("[%d]=%c\n",i, playerInput[i]);        // (Посимвольный вывод playerInput)
-        if(!strchr(/*getRandomWord()*/randomWord,playerInput[i])){  // ⚠️ Чувствителен к регистру, капсить все слова | Поиск (в строке, символа)
+        printf("[%d]=%c\n",i, playerInput[i]);        // (Посимвольный вывод playerInput) // ⚠️
+        if(!strchr(/*getRandomWord()*/randomWord,playerInput[i])){  // [!] Чувствителен к регистру, капсить все слова | Поиск (в строке, символа)
             printf("В слове %s не содержится буква %c или их количество меньше использованного\n",randomWord,playerInput[i]); // Изменить формат вывода
             isCorrect = 0;
             break;
         }
         else{
-            isCorrect = 1;
+            isCorrect = 1;  // iscorrect = 1 по умолчанию
         }
     }
     if(isCorrect==0){
@@ -59,20 +61,20 @@ char startGame(){
         char vocabularyStr[50];                         // *Строка из словаря
         int isFound = 0;
         while (fgets(vocabularyStr,50,vocabulary)) {    // *Запись в vocabularyStr строки из vocabulary(файла)
-            printf("%s",vocabularyStr);                 // ⚠️⚠️⚠️ Считывает строки из файла вместе с крансой строкой после слова => при сравнеии слова получаются разные => проверка всегда false
+            //printf("%s",vocabularyStr);                 // ⚠️⚠️⚠️ Считывает строки из файла вместе с крансой строкой после слова => при сравнеии слова получаются разные => проверка всегда false
             if(strcmp(vocabularyStr,playerInput)==0){   // *Сравнение слов, если идентичны, то возвращается 0
                 printf("[+] %s – подходит!\n",playerInput);
                 isFound++;
                 break;
             }
         }
-        if (isFound){
+        /*if (isFound){
             printf("%s Нашлось\n",vocabularyStr);       // *Слово существует в словаре
         }
         else{
             printf("Не нашлось\n");                     // *Слово не существует в словаре
-            printf("isFound?[%d], scrcmp=[%d], lastVocStr=[%s], input=[%s]\n",isFound, strcmp(vocabularyStr,playerInput),vocabularyStr,playerInput);
-        }
+            //printf("isFound?[%d], scrcmp=[%d], lastVocStr=[%s], input=[%s]\n",isFound, strcmp(vocabularyStr,playerInput),vocabularyStr,playerInput);
+        }*/
         fclose(vocabulary);
     }
     
