c90413e55786ae0064d0fe781d43476bf6039f56
dim4 <dmitrvls@gmail.com>
2022-05-13 21:26:52 +0600

    [1.5.8] Счетчик слов, вывод введенных слов, проверка на повтор, но первые два слова могут быть одинаковыми

--- typesetter/main.c
+++ typesetter/main.c
@@ -2,6 +2,7 @@
 //Для вывода %c CP866 в терминале или setlocale()
 //Для работы с Windows нужна CP866 + заменить"–" на "-"
 
+//Отдельная функция error, по коду ошибки выводить ее текст и перезапрашивать слово
 //Чтобы список слов не сбрасывался
 //Вызов printScore в doInput, чтобы он исчезал с очисткой экрана
 //Убрать sleep() для первого запуска
@@ -23,10 +24,64 @@
 
 int score;                                                  // *Счет игрока
 char randomWord[50];                                        // *Случайное слово
-//char playerInput[50];                                       // *Ввод игрока
 
+char clearScreen(){                                     // *Кроссплатформенная очистка экрана
+#if defined(_WIN32)
+    return system("cls");
+#elif defined(__linux__)
+    return system("clear");
+#elif defined(__APPLE__)
+    return system("clear");
+#endif
+}
+
+struct node{
+    char* word;
+    struct node* next;
+};
+typedef struct node list;
+list* head = NULL;
+
+list* addScore(list* head, char* word){
+    list* tmp = (list*) malloc(sizeof(list));
+    tmp->word  = word;
+    tmp->next  = head;
+    return tmp;
+}
+
+void printScore(list* head){
+    if(head){                               //Если передан ненулевой указатель
+        while(head->next){                  //Пока элемент не последний
+            printf("%s | ", head->word);    //Печатаем его word
+            head=head->next;                //И переходим к следующему
+        }
+        printf("%s\n", head->word);         //Печатаем последний элемент
+        score++;
+    }
+    else
+        printf("empty list\n");
+    printf("Введено слов: %d\n",score);
+    printf("_________________\n");
+}
+
+int alreadyUsed(list* head, char* word) {                    // *Проверка ввода ранее использованного слова
+    if(head){                               //Если передан ненулевой указатель
+        while(head->next){                  //Пока элемент не последний
+            if(strcmp(head->word,word)!=0){
+                head=head->next;            //И переходим к следующему
+            }
+            else{
+                return 1;
+            }
+        }
+        return 0;
+    }
+    else{
+        return 0;
+    }
+}
 /*
-repetitionCount(){
+void repetitionCount(){
     #include <stdio.h>
 #include <string.h>
 
@@ -47,51 +102,6 @@ int main(void) {
 }
 }*/
 
-char clearScreen(){                                     // *Кроссплатформенная очистка экрана
-#if defined(_WIN32)
-    return system("cls");
-#elif defined(__linux__)
-    return system("clear");
-#elif defined(__APPLE__)
-    return system("clear");
-#endif
-}
-
-
-
-
-struct node{
-    char* word;
-    struct node* next;
-};
-
-typedef struct node list;
-
-list *head = NULL;
-
-list* addScore(list* head, char* word){
-    list* tmp = (list*) malloc(sizeof(list));
-    tmp->word  = word;
-    tmp->next  = head;
-    return tmp;
-}
-
-void printScore(list* head){
-    if(head){                               //Если передан ненулевой указатель
-        while(head->next){                  //Пока элемент не последний
-            printf("%s | ", head->word);    //Печатаем его id
-            head=head->next;                //И переходим к следующему
-        }
-        printf("%s\n", head->word);         //Печатаем последний элемент
-        score++;
-    }
-    else
-        printf("empty list\n");
-    printf("Введено слов: %d\n",score);
-    printf("_________________\n");
-}
-
-
 void doInput();                                             // *Прототип функции для рекурсии
 
 void checkInput(char* playerInput){
@@ -109,8 +119,7 @@ void checkInput(char* playerInput){
             }
         }
         if(isCorrect!=0){
-            // Проверка существования слова:
-            //printf("Слово \x1b[4m%s\x1b[0m\x1b[32m подходит\x1b[0m по буквам.\n",playerInput);
+            // *Проверка существования слова:
             FILE *vocabulary;
             vocabulary = fopen("/Users/dmitry/Desktop/ОмГУПС/typesetter/russian.txt","r");
             char vocabularyStr[50];                         // *Строка из словаря
@@ -118,24 +127,29 @@ void checkInput(char* playerInput){
                 fscanf(vocabulary, "%s",vocabularyStr);
                 if(strcmp(playerInput, vocabularyStr)==0){  // *Сравнение слов, если идентичны, то возвращается 0
                     fclose(vocabulary);
-                    printf("[+1] Cлово \x1b[4m%s\x1b[0m \x1b[32mподходит!\x1b[0m\n",playerInput);
-                    sleep(1);
-                    clearScreen();
-                    head = addScore(head, playerInput);
-                    printScore(head);
-                    doInput();
+                    //printf("\x1b[35maU returned %d\x1b[0m\n",alreadyUsed(head,playerInput));
+                    if(alreadyUsed(head,playerInput)==0){
+                        printf("[+1] Cлово \x1b[4m%s\x1b[0m \x1b[32mподходит!\x1b[0m\n",playerInput);
+                        sleep(1);
+                        clearScreen();
+                        head = addScore(head, playerInput);
+                        printScore(head);
+                        doInput();
+                    } else{
+                        printf("Вы \x1b[31mуже использовали\x1b[0m это слово\n");
+                        sleep(1);
+                        clearScreen();
+                        doInput();
+                    }
                 }
             }
             printf("Слово \x1b[4m%s\x1b[0m \x1b[31mне существует!\x1b[0m\n",playerInput);
             sleep(1);
             clearScreen();
             doInput();
-            //Текст не выводится, если нет \n на конце, который вроде там не нужен.
-            //Сделать возврат к вводу
         }
     }
     else{
-        isCorrect = 0;
         printf("Вводимое слово \x1b[31mдолжно отличаться\x1b[0m от слова \x1b[4m%s\x1b[0m\n",randomWord);
         sleep(1);
         clearScreen();
@@ -196,7 +210,7 @@ void menu(){
         printf("Допускается располагать буквы в любом порядке, однако повторяться они могут столько раз, сколько содержатся в исходном слове.\n");
         printf("При проверке слов учитывается регистр.\n");
         printf("\n/menu – вернуться в меню\n");
-        printf(": ");
+        printf(":");
         scanf("%s",input);
         if (strcmp("/menu",input)==0){
             menu();
@@ -222,7 +236,7 @@ int main() {
     //setlocale(LC_ALL, "utf-8");
     //char vocabularyPath[60] = {"/Users/dmitry/Desktop/ОмГУПС/typesetter/russian.txt"};  // [!] Путь к файлу в переменную
     srand(time(NULL));
-    menu();
+    //menu();
     //Для быстрого старта
-    //startGame();
+    startGame();
 }
